6e7bfd49c92eee2b4cac032c92a0f2f5e05ee386
Alex Romayev <romayev@gmail.com>
2016-11-28 11:40:36 -0500

    Update images and more.

--- apps/Ingredient Selector/Ingredient Selector/Classes/View/ISMoreViewController.m
+++ apps/Ingredient Selector/Ingredient Selector/Classes/View/ISMoreViewController.m
@@ -9,7 +9,9 @@
 #import "ISMoreViewController.h"
 
 @interface ISMoreViewController ()
-
+@property (strong, nonatomic) IBOutlet UILabel *aboutLabel;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *descLabel;
 @end
 
 @implementation ISMoreViewController
@@ -17,6 +19,9 @@
 - (void) viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = NSLocalizedString(@"ingredion", nil);
+    _aboutLabel.text = NSLocalizedString(@"company.meet-company", nil);
+    _titleLabel.text = NSLocalizedString(@"company.locations-title", nil);
+    _descLabel.text = NSLocalizedString(@"company.locations-desc", nil);
 }
 
 @end
