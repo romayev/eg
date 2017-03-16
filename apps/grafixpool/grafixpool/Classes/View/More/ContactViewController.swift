87b38a6cd6af3fe54801f54e14ac496c74234b4c
Alex Romayev <romayev@gmail.com>
2017-03-15 22:31:12 -0400

    Implement state restoration. Implement contact view. Build #2.

---
+++ apps/grafixpool/grafixpool/Classes/View/More/ContactViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ContactViewController.swift
+//  grafixpool
+//
+//  Created by Alex Romayev on 3/15/17.
+//  Copyright © 2017 Executive Graphics. All rights reserved.
+//
+
+import UIKit
+import EGKit
+
+class ContactViewController: EGViewController {
+    @IBOutlet var title1Label: UILabel!
+    @IBOutlet var title2Label: UILabel!
+    @IBOutlet var textView1: UITextView!
+    @IBOutlet var textView2: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = NSLocalizedString("contact", comment: "")
+        var inset = textView1.contentInset
+        inset.left -= 6
+        inset.right -= 6
+        textView1.contentInset = inset
+        textView2.contentInset = inset
+    }
+}
